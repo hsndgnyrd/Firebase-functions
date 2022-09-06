6e270a44c49009caa22a699cf5eff895d51638ad
hsndgnyrd <hasandugunyurdu27@gmail.com>
2022-09-06 22:12:50 +0300

    Create user function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import "./App.css";
+import React, { useState } from "react";
+import { createUser } from "./firebase";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const user = await createUser(email, password);
+    console.log(user);
+  };
+
   return (
     <div>
       <h1>Hi Firebase</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Kayıt</button>
+      </form>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,3 +13,11 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth();
+
+// AUTH
+// CREATE New User by mail and password
+export const createUser = async (email, password) => {
+  const { user } = await createUserWithEmailAndPassword(auth, email, password);
+  return user;
+};
